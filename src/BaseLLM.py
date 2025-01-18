8bb5e290736994da576df65f8d10e6fd9d45c80e
Radon10043 <772577952@qq.com>
2025-01-18 20:39:37 +0800

    🦄 refactor: Update params of funcs in BaseLLM

--- src/BaseLLM.py
+++ src/BaseLLM.py
@@ -1,25 +1,66 @@
 from utils import *
 
+
 class BaseLLM:
     """Base LLM Class"""
 
-    def __init__(self):
-        """构造函数"""
-        FATAL("This is a base class, please use the derived class instead.")
+    def __init__(self, base_url: str, api_key: str, model: str, temperature: float = 0.5):
+        """构造函数, 初始化LLM对象
 
+        Parameters
+        ----------
+        base_url : str
+            API的基础URL
+        api_key : str
+            API Key
+        model : str
+            要调用的模型
+        temperature : float, optional
+            温度参数, by default 0.5
+        """
+        FATAL("BaseLLM is an abstract class, please use other class instead.")
 
-    def append_sys_prompt(self):
-        """设置系统提示信息"""
-        FATAL("This is a base class, please use the derived class instead.")
+    def append_sys_prompt(self, sys_prompt: str):
+        """设置系统提示信息, 感觉大部分情况下系统提示一条就够了...?
 
-    def chat(self) -> str:
-        """对话"""
-        FATAL("This is a base class, please use the derived class instead.")
+        Parameters
+        ----------
+        sys_prompt : str
+            系统提示信息
+        """
+        FATAL("BaseLLM is an abstract class, please use other class instead.")
+
+    def chat(self, user_prompt: str) -> str:
+        """对话
+
+        Parameters
+        ----------
+        user_prompt : str
+            用户提示信息
+
+        Returns
+        -------
+        str
+            LLM回复的信息
+        """
+        FATAL("BaseLLM is an abstract class, please use other class instead.")
 
     def avaliable_models(self) -> list:
-        """列出所有可用的模型"""
-        FATAL("This is a base class, please use the derived class instead.")
+        """列出所有可用的模型
 
-    def save_messages(self):
-        """保存对话信息, 支持保存为json文件或md文件"""
-        FATAL("This is a base class, please use the derived class instead.")
+        Returns
+        -------
+        list
+            可用的模型列表
+        """
+        FATAL("BaseLLM is an abstract class, please use other class instead.")
+
+    def save_messages(self, path: str):
+        """保存对话信息, 支持保存为json文件或md文件
+
+        Parameters
+        ----------
+        path : str
+            消息记录保存路径
+        """
+        FATAL("BaseLLM is an abstract class, please use other class instead.")
