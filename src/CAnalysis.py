57e82ac8cb763264fd1493fe629b35eff824a4c5
Radon10043 <772577952@qq.com>
2025-03-03 17:48:47 +0800

    ✨ feat: Update CAnalysis.py to print syscalls and its line

--- src/CAnalysis.py
+++ src/CAnalysis.py
@@ -5,27 +5,50 @@ import os
 from clang.cindex import Config, Cursor, CursorKind, Token, TokenKind, Index
 
 
-def visit(cursor: Cursor):
-    """访问C语言代码中的每一个节点
+# ========== GLOBAL VARIABLES ==========
+SYSCALL_LIST = list()  # 系统调用列表
+# ======================================
+
+
+def getCallExpr(cursor: Cursor):
+    """获取当前节点下的所有函数调用
 
     Parameters
     ----------
     cursor : Cursor
         当前节点
     """
-    if cursor.location.is_in_system_header:
-        return
-    if cursor.kind == CursorKind.CALL_EXPR and len(cursor.spelling):
-        last_line = cursor.location.line
-        for token in cursor.get_tokens():
-            last_line = token.location.line
-        print("[CALL_EXPR]:", cursor.spelling, "[LAST_LINE]:", last_line)
+    global SYSCALL_LIST
     for child in cursor.get_children():
-        visit(child)
+        if child.kind == CursorKind.CALL_EXPR and child.spelling != "":
+            # 当遇到syscall时, 记录其名称和行号, 组成元组保存至SYSCALL_LIST
+            syscall_name = child.spelling
+            last_line = child.location.line
+            for token in child.get_tokens():
+                last_line = token.location.line
+            SYSCALL_LIST.append((syscall_name, last_line))
+        getCallExpr(child)
+
+
+def getSyscallsFromMain(cursor: Cursor):
+    """获取main函数中的每个系统调用, 对应了syzkaller生成的种子文件的每个系统调用
+
+    Parameters
+    ----------
+    cursor : Cursor
+        当前节点
+    """
+    # 遍历AST的节点
+    for child in cursor.get_children():
+        if child.location.is_in_system_header:
+            continue
+        # 若当前节点是函数声明节点且函数名为main, 访问该节点, 递归遍历其子节点
+        if child.kind == CursorKind.FUNCTION_DECL and child.spelling == "main":
+            getCallExpr(child)
 
 
 def main(args: argparse.Namespace):
-    """分析获取C文件中所有函数的调用位置
+    """分析获取C文件main函数中所有系统调用的位置
 
     Parameters
     ----------
@@ -37,7 +60,11 @@ def main(args: argparse.Namespace):
     Config.set_library_file(libclang_path)
     index = Index.create()
     tu = index.parse(args.file)
-    visit(tu.cursor)
+    getSyscallsFromMain(tu.cursor)
+
+    # 打印syscall的名称和最后行号
+    for syscall in SYSCALL_LIST:
+        print(f"{syscall[0]},{syscall[1]}")
 
 
 if __name__ == "__main__":
