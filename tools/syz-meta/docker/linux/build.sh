f93228f5c63e2876c4f43f547c9665dcca6c8fed
Radon10043 <772577952@qq.com>
2025-05-16 20:49:40 +0800

    🔧 build(tools/syz-meta): Update build.sh for version and configuration handling

--- tools/syz-meta/docker/linux/build.sh
+++ tools/syz-meta/docker/linux/build.sh
@@ -9,7 +9,13 @@ pushd $1
 make defconfig
 make kvm_guest.config
 if [ "$2" == "syzbot" ]; then
-    cp ../syzbot.config .config
+    if [ "$1" == "v5.15" ]; then
+        cp ../syzbot.v5.15.config .config
+    else if [ "$1" == "v6.1" ]; then
+        cp ../syzbot.v6.1.config .config
+    else
+        cp ../syzbot.config .config
+    fi
 else
     echo -e 'CONFIG_KCOV=y\nCONFIG_DEBUG_INFO_DWARF4=y\nCONFIG_KASAN=y\nCONFIG_KASAN_INLINE=y\nCONFIG_CONFIGFS_FS=y\nCONFIG_SECURITYFS=y\nCONFIG_CMDLINE_BOOL=y\nCONFIG_CMDLINE="net.ifnames=0"' > .config
 fi
