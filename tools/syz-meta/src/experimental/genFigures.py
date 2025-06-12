84dc14f726e6d9fe6f9aedd1e83eac1a3d7072b5
Radon10043 <772577952@qq.com>
2025-06-12 18:01:02 +0800

    ✨ feat(tools/syz-meta): Add MR iden result plotting

---
+++ tools/syz-meta/src/experimental/genFigures.py
@@ -0,0 +1,111 @@
+import argparse
+import os
+
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import matplotlib.ticker as mticker
+
+
+########## GLOBAL VARIABLES ##########
+FONT_SIZE = 14
+FONT = "Consolas"
+######################################
+
+
+def genMRIdenFig(args: argparse.Namespace):
+    """Generate figure of MR identification results.
+
+    Parameters
+    ----------
+    args : argparse.Namespace
+        Command line arguments.
+    """
+    excel = args.excel
+    df = pd.read_excel(excel, sheet_name="Metamorphic", header=1, engine="calamine")
+
+    # Get the unique kernel versions, excluding "general"
+    tmp = df["kernel version"].unique()
+    indices = np.where(tmp != "general")
+    vers = tmp[indices].tolist()
+    vers.sort()
+
+    # Get the unqiue drivers
+    drivers = df["driver"].unique().tolist()
+
+    # Initialize the plot
+    plt.rcParams.update({"font.size": FONT_SIZE, "font.family": FONT})
+    fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(12, 8))
+    axes = axes.flatten()
+
+    # Draw the figure of MR identification result
+    for i in range(len(vers)):
+        ver = vers[i]
+        plot_data_list = list()
+        for driver in drivers:
+            df_driver = df[df["driver"] == driver]
+            df_total = df_driver[(df_driver["kernel version"] == ver) | (df_driver["kernel version"] == "general")]
+            df_gen_succ = df_total[df_total["iden succ"] & df_total["impl succ"]]
+            df_legal = df_gen_succ[df_gen_succ["legal"] == True]
+            df_correct = df_legal[df_legal["correct"] == True]
+            cnt_total, cnt_gen_succ, cnt_legal, cnt_correct = len(df_total), len(df_gen_succ), len(df_legal), len(df_correct)
+            plot_data_list.append({"driver": driver, "type": "gen_succ", "count": cnt_gen_succ, "rate": cnt_gen_succ / cnt_total})
+            plot_data_list.append({"driver": driver, "type": "legal", "count": cnt_legal, "rate": cnt_legal / cnt_total})
+            plot_data_list.append({"driver": driver, "type": "correct", "count": cnt_correct, "rate": cnt_correct / cnt_total})
+
+        # Create the figure
+        plot_data = pd.DataFrame(plot_data_list)
+        sns.barplot(x="driver", y="rate", hue="type", data=plot_data, palette="muted", ax=axes[i])
+        axes[i].set_title("(" + chr(ord("a") + i) + f") {ver}")
+
+        # Format y-axis as percentage
+        formatter = mticker.PercentFormatter(xmax=1.0, decimals=0)
+        axes[i].yaxis.set_major_formatter(formatter)
+
+        # Set bar properties
+        for index, bar in enumerate(axes[i].patches):
+            # Set edge color to black
+            bar.set_edgecolor("black")
+            bar.set_linewidth(1)
+
+            # Invalid bar check
+            if bar.get_xy() == (0, 0):  # ?
+                continue
+
+            # Get corresponding count of the bar
+            driver = plot_data["driver"].unique()[index % len(drivers)]
+            rate_type = plot_data["type"].unique()[(index // len(drivers)) % 3]
+            cnt = plot_data[(plot_data["driver"] == driver) & (plot_data["type"] == rate_type)]["count"].values[0]
+
+            # Write text in/on the top of the bar
+            text = f"{cnt}, {bar.get_height():.2%}"
+            x = bar.get_x() + bar.get_width() / 2
+            y = bar.get_height()
+            if y <= 0.2:
+                y += 0.06 + len(text) * 0.01
+            else:
+                y -= 0.06 + len(text) * 0.01
+            axes[i].text(x, y, text, ha="center", va="center", rotation=90, color="black", fontsize=FONT_SIZE - 4)
+
+        # Set the x and y labels
+        axes[i].set_xlabel("Drivers")
+        axes[i].set_ylabel("Rate")
+
+    # Set the legend
+    handles, labels = axes[0].get_legend_handles_labels()
+    for ax in axes:
+        ax.get_legend().remove()
+    fig.legend(handles, labels, title="Type", loc="lower center", ncols=3)
+
+    # Adjust layout and save the figure
+    plt.tight_layout(rect=(0, 0.06, 1, 1))
+    plt.savefig(f"bar.pdf")
+    print("Successfully draw MR identification result figure.")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Draw plots from data")
+    parser.add_argument("--excel", type=str, required=True, help="Path to the Excel file containing data")
+    args = parser.parse_args()
+    genMRIdenFig(args)
