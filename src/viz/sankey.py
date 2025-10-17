b9b9f8ccb3c05ba7e35a5c24dd5f91b74f86010c
Radon10043 <772577952@qq.com>
2025-10-17 11:14:19 +0800

    ✨ feat(src/viz): Add annotations to sankey

--- src/viz/sankey.py
+++ src/viz/sankey.py
@@ -26,15 +26,16 @@ android_lq_highvio  = df[df["kernel"] == "Android"]["EKMR-low-quality-highvio"].
 data = dict(
     node_labels=[
         "Document Chunks",
-        "Linux",
-        "Android",
+        "Document Chunks<br>(Linux)",
+        "Document Chunks<br>(Android)",
         "KMR",
         "No Consensus",
         "EKMR",
         "Generation Failed",
-        "High-Quality EKMRs",
-        "Low-Quality EKMRs<br>(No Coverage)",
-        "Low-Quality EKMRs<br>(High Violation Rate)",
+        # To put these label to the right of node, we add them manually as annotations
+        # "High-Quality EKMRs",
+        # "Low-Quality EKMRs<br>(No Coverage)",
+        # "Low-Quality EKMRs<br>(High Violation Rate)",
     ],
     node_colors=[
         "rgba(213,94,0,0.6)",
@@ -90,6 +91,125 @@ data = dict(
     ],
 )
 
+annotations = [
+    dict(   # Node label of [High-Quality EKMRs]
+        x=1.22,
+        y=0.7,
+        text="High-Quality EKMRs",
+        showarrow=False,
+    ),
+    dict(   # Node label of [Low-Quality EKMRs (No Coverage)]
+        x=1.22,
+        y=0.2,
+        text="Low-Quality EKMRs<br>(No Coverage)",
+        showarrow=False,
+    ),
+    dict(   # Node label of [Low-Quality EKMRs (High Violation Rate)]
+        x=1.23,
+        y=0.00,
+        text="Low-Quality EKMRs<br>(High Violation Rate)",
+        showarrow=False,
+    ),
+    dict(   # Flow value of [doc chunks] -> [doc chunks (linux)]
+        x=0.1,
+        y=0.7,
+        text=str(linux_chunks),
+        font=dict(size=12, shadow="auto"),
+        showarrow=False,
+    ),
+    dict(   # Flow value of [doc chunks] -> [doc chunks (android)]
+        x=0.1,
+        y=0.2,
+        text=str(android_chunks),
+        font=dict(size=12, shadow="auto"),
+        showarrow=False,
+    ),
+    dict(   # Flow value of [doc chunks (linux)] -> [KMR]
+        x=0.27,
+        y=0.65,
+        text=str(linux_kmr),
+        font=dict(size=12, shadow="auto"),
+        showarrow=False,
+    ),
+    dict(   # Flow value of [doc chunks (linux)] -> [No Consensus]
+        x=0.27,
+        y=0.53,
+        text=str(linux_chunks - linux_kmr),
+        font=dict(size=12, shadow="auto"),
+        showarrow=False,
+    ),
+    dict(   # Flow value of [doc chunks (android)] -> [KMR]
+        x=0.27,
+        y=0.1,
+        text=str(android_kmr),
+        font=dict(size=12, shadow="auto"),
+        showarrow=False,
+    ),
+    dict(   # Flow value of [doc chunks (android)] -> [No Consensus]
+        x=0.35,
+        y=0.01,
+        text=str(android_chunks - android_kmr),
+        font=dict(size=12, shadow="auto"),
+        showarrow=False,
+    ),
+    dict(   # Flow value of [KMR] -> [EKMR] (linux)
+        x=0.6,
+        y=0.7,
+        text=str(linux_ekmr),
+        font=dict(size=12, shadow="auto"),
+        showarrow=False,
+    ),
+    dict(   # Flow value of [KMR] -> [EKMR] (android)
+        x=0.6,
+        y=0.4,
+        text=str(android_ekmr),
+        font=dict(size=12, shadow="auto"),
+        showarrow=False,
+    ),
+    dict(   # Flow value of [KMR] -> [Generation Failed] (linux/android)
+        x=0.62,
+        y=0.15,
+        text=f"{linux_kmr - linux_ekmr} / {android_kmr - android_ekmr}",
+        font=dict(size=12, shadow="auto"),
+        showarrow=False,
+    ),
+    dict(   # Flow value of [EKMR] -> [High-Quality EKMRs] (linux)
+        x=0.9,
+        y=0.83,
+        text=f"{linux_hq}",
+        font=dict(size=12, shadow="auto"),
+        showarrow=False,
+    ),
+    dict(   # Flow value of [EKMR] -> [High-Quality EKMRs] (android)
+        x=0.9,
+        y=0.65,
+        text=f"{android_hq}",
+        font=dict(size=12, shadow="auto"),
+        showarrow=False,
+    ),
+    dict(   # Flow value of [EKMR] -> [Low-Quality EKMRs (No Coverage)] (linux)
+        x=0.9,
+        y=0.45,
+        text=f"{linux_lq_nocov}",
+        font=dict(size=12, shadow="auto"),
+        showarrow=False,
+    ),
+    dict(   # Flow value of [EKMR] -> [Low-Quality EKMRs (No Coverage)] (android)
+        x=0.9,
+        y=0.2,
+        text=f"{android_lq_nocov}",
+        font=dict(size=12, shadow="auto"),
+        showarrow=False,
+    ),
+    dict(   # Flow value of [EKMR] -> [Low-Quality EKMRs (High Violation Rate)] (linux/android)
+        x=0.95,
+        y=0.05,
+        text=f"{linux_lq_highvio} / {android_lq_highvio}",
+        font=dict(size=12, shadow="auto"),
+        showarrow=False,
+    ),
+]
+
 fig = go.Figure(
     data=go.Sankey(
         node=dict(
@@ -107,14 +227,17 @@ fig = go.Figure(
             value=data["link_values"],
             color=data["link_colors"],
         ),
-    )
+    ),
 )
 
+for ann in annotations:
+    fig.add_annotation(ann)
+
 fig.update_layout(
-    width=800,
+    width=1000,
     height=400,
-    font=dict(size=16),
-    margin=dict(l=10, r=10, t=10, b=10),
+    font=dict(size=16, color="black"),
+    margin=dict(l=10, r=200, t=10, b=10),
 )
 
 outdir = Path(__file__).parent / "output"
